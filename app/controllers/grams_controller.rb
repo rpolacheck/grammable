ba947624ed09941fcba0292b18472ebca714fe5a
Robert Polacheck <rpolacheck@gmail.com>
2017-06-14 00:30:22 +0000

    Added carrier wave and heroku functionality

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -36,6 +36,7 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destro
   end
 
   def index
+    @grams = Gram.all
   end
 
   def show
@@ -52,7 +53,7 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destro
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 
   def render_not_found(status=:not_found)
